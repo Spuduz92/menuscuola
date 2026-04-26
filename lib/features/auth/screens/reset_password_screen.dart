bc03ec56e377d6951bd151c09912c98a861ca921
Spuduz92 <mickey.spadafora@gmail.com>
2026-04-26 19:08:35 +0200

    dd

--- lib/features/auth/screens/reset_password_screen.dart
+++ lib/features/auth/screens/reset_password_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 import '../../../app/theme.dart';
 import '../../../core/supabase/supabase_client.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
